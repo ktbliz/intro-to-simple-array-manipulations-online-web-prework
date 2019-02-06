62fdebdf7c35f26803c425c34841a03667c1e194
Katie Blizzard <kblizzard11@gmail.com>
2019-02-06 02:34:21 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,43 @@
+def using_push(array, new_string)
+
+def using_unshift(array, new_string)
+
+def using_pop
+
+end 
+
+def pop_with_args(array, num_elements_to_remove)
+
+end
+
+def using_shift(array)
+
+end 
+
+def shift_with_args, num_elements_to_remove)
+
+end 
+
+def using_concat(array1, array2)
+
+end 
+
+def using_insert(array, new_element)
+  array.insert at 4th position
+end 
+
+def using_uniq(array)
+
+end
+
+def using_flatten(array)
+
+end 
+
+def using_delete(array, string)
+
+end
+
+def using_delete_at(array, integer)
+   
+end
